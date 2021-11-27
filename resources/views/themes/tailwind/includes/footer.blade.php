e29ab1b4a3dd1c5b11a6cfcbd1d8ef72b4b25628
sondhAgileTech <son.dh@hesperiafreepress.com>
2021-11-27 15:29:31 +0700

    fix bugs

--- resources/views/themes/tailwind/includes/footer.blade.php
+++ resources/views/themes/tailwind/includes/footer.blade.php
@@ -4,11 +4,11 @@
             <h2 class="visually-hidden" id="block-pegi-footer-menu">Footer menu</h2>
             <ul>
                 <li>
-                    <a href="/about">Cổng thông tin tra cứu pân loại game theo lứa tuổi</a>
+                    <a href="/about">Cổng thông tin tra cứu phân loại game theo lứa tuổi</a>
                 </li>
-                <li>
+                {{-- <li>
                     <a href="/about">Phát triển và vận hành bởi NISCI</a>
-                </li>
+                </li> --}}
                 {{-- <li>
                     <a href="/privacy" title="Privacy policy">Chính sánh</a>
                 </li> --}}
